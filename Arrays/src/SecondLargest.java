2a28e8e1534c54331877d0ae4a2203d6af425f38
Shrinath-Ghorpade <shrinathghorpade07@gmail.com>
2026-02-15 12:02:08 +0530

    Add second largest soultion

---
+++ Arrays/src/SecondLargest.java
@@ -0,0 +1,20 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr={2,5,3,7,8,9,10,1};
+        int largest=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                second=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>second && second<largest){
+                second=arr[i];
+            }
+
+
+        }
+        System.out.println("Second largest elemnet is "+ second);
+
+    }
+}
