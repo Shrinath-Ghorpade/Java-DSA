8631295d0a8799668739b00ff20f19fe1a31ab0e
Shrinath-Ghorpade <shrinathghorpade07@gmail.com>
2026-02-24 19:34:55 +0530

    Add anagram solution

---
+++ String/src/Anagrams.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        String str1="Listen";
+        String str2="Silent";
+
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        if(str1.length()==str2.length()){
+            char[] arr1=str1.toCharArray();
+            char[] arr2=str2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            boolean result=Arrays.equals(arr1,arr2);
+
+            if(result){
+              System.out.println("The strings are anagrams");
+            }else{
+                System.out.println("The strings are not anagrams");
+            }
+        }
+    }
+}
