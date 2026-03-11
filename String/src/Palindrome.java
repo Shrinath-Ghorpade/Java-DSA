67bb62a4bf2c765eff23878c7cb56be761a59c1d
Shrinath-Ghorpade <shrinathghorpade07@gmail.com>
2026-03-11 19:53:51 +0530

    Add string problems

--- String/src/Palindrome.java
+++ String/src/Palindrome.java
@@ -24,7 +24,24 @@ public class Palindrome {
 
     public static void main(String[] args) {
         String str="madam";
-        System.out.println(checkPalindrome(str));
+        // System.out.println(checkPalindrome(str));
+        int left=0;
+        int right=str.length()-1;
+
+        boolean isPalindrome=true;
+
+        while (left<right) {
+            if(str.charAt(left)!=str.charAt(right)){
+                isPalindrome=false;
+            }
+            left++;
+            right--;
+        }
+        if(isPalindrome){
+            System.out.println("Palindrom");
+        }else{
+            System.out.println("Not palindrome");
+        }
     }
 }
 
